b79f4501efa505146b1972c39755348213c7c511
elandi <56363232+Elandi-rj@users.noreply.github.com>
2019-12-20 21:45:27 +1100

    overhaul, changed from simple blocks to mesh

--- ripples/ripples.js
+++ ripples/ripples.js
@@ -1,112 +1,94 @@
+let cols = 100; //rows
+const sclfactor = 30;
+const scl = sclfactor / (cols / (cols / 2));
+const center = sclfactor * (cols - 1) / 4;
+let arrBlocks = new Array(cols);
+let arrBuffer = new Array(cols);
+for (let i = 0; i < arrBlocks.length; i++) {
+    arrBlocks[i] = new Array();
+    arrBuffer[i] = new Array();
+    for (let j = 0; j < cols; j++) {
+        arrBlocks[i][j] = 0;
+        arrBuffer[i][j] = 0;
+    }
+}
 
-var cols = 36;
-var rows = 36;
-var angle = 0.90;
-const strengthMultiplier = -200;
-const strengthDampening = 0.99;
-
-
-let arrBlocks = new Array(rows);
-let arrBuffer = new Array(rows);
-var mystring = "";
 function setup() {
-    createCanvas(windowWidth, windowHeight, WEBGL);
-    for (var i = 0; i < arrBlocks.length; i++) {
-        arrBlocks[i] = new Array(2);
-        arrBuffer[i] = new Array(2);
-    }
+    createCanvas(windowWidth, windowHeight - 54, WEBGL);
+    output.innerHTML = 100 - slider.value + "%";
+}
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight - 54);
+}
 
-    for (var i = 0; i < rows; i++) {
-        for (var j = 0; j < cols; j++) {
-            arrBlocks[i][j] = new Block();
-            arrBuffer[i][j] = new Block();
-        }
-    }
-    /*
-        for (var i = 1; i < 5; i++) {
-            for (var j = 1; j < 5; j++) {
-                arrBlocks[i][j].RainValue = strengthMultiplier;
-    
-            }
-        }
-    */
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+var strengthDampening = slider.value / 100;
+//Dampening slider
+slider.oninput = function () {
+    output.innerHTML = 100 - this.value + "%";
+    strengthDampening = slider.value / 100;
+}
+
+var sliderStrength = document.getElementById("myStrengthRange");
+var outputstrength = document.getElementById("demoStrength");
+outputstrength.innerHTML = sliderStrength.value;
+var strengthMultiplier = sliderStrength.value - 0;;
+//strength slider
+sliderStrength.oninput = function () {
+    outputstrength.innerHTML = this.value;
+    strengthMultiplier = sliderStrength.value - 0;
 }
 
 function draw() {
     background(51);
-    //ambientLight(51, 102, 200);
-    //pointLight(250, 60, 70, 200, -200, 400);
-    //angle += 0.005;
-    rotateX(angle);
-    //translate(150, -250, -800);
-    translate(250, -600, -200);
-    for (var i = 1; i < rows - 1; i++) {
+    rotateX(PI / 3);
+    translate(-center, -800 - 400, -200);
+    frameRate(60);
+    noStroke();
+    for (var i = 1; i < cols - 1; i++) {
+        beginShape(TRIANGLE_STRIP);
         for (var j = 1; j < cols - 1; j++) {
-
-            arrBuffer[i][j].RainValue =
-                ((arrBlocks[i - 1][j].RainValue +
-                    arrBlocks[i + 1][j].RainValue +
-                    arrBlocks[i][j + 1].RainValue +
-                    arrBlocks[i][j - 1].RainValue) / 2 - arrBuffer[i][j].RainValue);
-
-            arrBuffer[i][j].RainValue = arrBuffer[i][j].RainValue * strengthDampening;
-
-            arrBlocks[i][j].drawboy(((i * 40) - width / 2), ((j * 40) - height / 2));
-            rectMode(CENTER);
-            arrBlocks[i][j].height = arrBlocks[i][j].RainValue;
+            fill(color(arrBlocks[j][i] + 50, arrBlocks[j][i] + 50, 150 + arrBlocks[j][i]));
+            arrBuffer[i][j] =
+                ((arrBlocks[i - 1][j] +
+                    arrBlocks[i + 1][j] +
+                    arrBlocks[i][j + 1] +
+                    arrBlocks[i][j - 1]) / 2 - arrBuffer[i][j]);
+            arrBuffer[i][j] = arrBuffer[i][j] * strengthDampening;
+            vertex(j * scl, i * scl, arrBlocks[j][i]);
+            vertex(j * scl, (i + 1) * scl, arrBlocks[j][i + 1]);
         }
+        endShape();
     }
     var arrTemp = arrBlocks;
     arrBlocks = arrBuffer;
     arrBuffer = arrTemp;
 
-
     let x = Math.round(map(mouseX, 0, windowWidth, 1, cols - 6));
-    let y = Math.round(map(mouseY, 0, windowHeight, 1, rows - 6));
+    let y = Math.round(map(mouseY, 0, windowHeight - 50, 1, cols - 6));
 
-
-    if (mouseIsPressed) {
+    if (mouseIsPressed && mouseX < windowWidth && mouseY >= 0 && mouseX >= 0) {
         for (let i = 0; i < 5; i++) {
             for (var j = 1; j < 5; j++) {
-                arrBlocks[x + i][y + j].RainValue = strengthMultiplier;
+                arrBlocks[x + i][y + j] = strengthMultiplier;
             }
         }
     }
 }
-
-class Block {
-    constructor() {
-        this.size = 40;
-        this.height;
-        this.RainValue = 0;
-    }
-    drawboy(posx, posy) {
-        noStroke();
-        push();
-        translate(posx, posy, this.height);
-        fill(color(this.RainValue + 50, this.RainValue + 50, 150 + this.RainValue));
-        box(this.size);
-        pop();
-    }
-
-    growboy(growthnum) {
-        this.size += growthnum;
-    }
-}
-
 function resetArray() {
-    for (var i = 0; i < rows; i++) {
+    for (var i = 0; i < cols; i++) {
         for (var j = 0; j < cols; j++) {
 
-            arrBuffer[i][j].RainValue = 0;
-            arrBlocks[i][j].RainValue = 0;
+            arrBuffer[i][j] = 0;
+            arrBlocks[i][j] = 0;
         }
     }
 }
 function boost() {
-    for (var i = 1; i < rows - 1; i++) {
+    for (var i = 1; i < cols - 1; i++) {
         for (var j = 1; j < cols - 1; j++) {
-            arrBuffer[i][j].RainValue = 0;
+            arrBuffer[i][j] = 0;
         }
     }
 }
